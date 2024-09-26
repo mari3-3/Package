4cc62609b9b11e8c894a71adeb12759a44184ddb
mari3-3 <0somethingsomething0@gmail.com>
2024-09-26 08:18:58 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+print("Please fill out your information for your package:")
+
+first_name = input ("Please type your first name:")
+middle_int = input ("Then your middle intial:")
+last_name = input ("Now type your last name:")
+street_address = input ("Please enter your street address:")
+city = input ("Please enter your city:")
+state = input ("Please enter your state:")
+zip_code = input ("Please enter your zip_code:")
+phone_number = input ("Please enter your phone_number:")
+email_address = input ("Please enter your email address:")
+
+print()
+print(f"{first_name} {middle_int} {last_name}")
+print(f"{street_address}")
+print(f"{city}, {state} {zip_code}")
+print (f"{phone_number}")
+print(f"{email_address}")
